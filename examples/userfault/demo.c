40ebd49be733bfd8dd14142817a6f469f37f6227
eliorgigi <eliorgigi1@gmail.com>
2022-04-22 07:54:11 +0000

    separete dm-handler by PF cases

--- examples/userfault/demo.c
+++ examples/userfault/demo.c
@@ -22,20 +22,14 @@
 
        #define errExit(msg)    do { perror(msg); exit(EXIT_FAILURE); \
                                } while (0)
+       #define NUM_OF_PAGES 3
 
        static int page_size;
 
-       static void *
-       fault_handler_thread(void *arg)
+       void handle_page_fault_event(struct uffd_msg* msg, long uffd)
        {
-           static struct uffd_msg msg;   /* Data read from userfaultfd */
-           static int fault_cnt = 0;     /* Number of faults so far handled */
-           long uffd;                    /* userfaultfd file descriptor */
            static char *page = NULL;
            struct uffdio_copy uffdio_copy;
-           ssize_t nread;
-
-           uffd = (long) arg;
 
            /* Create a page that will be copied into the faulting region. */
 
@@ -46,73 +40,71 @@
                    errExit("mmap");
            }
 
+           /* Display info about the page-fault event. */
+
+           printf("    UFFD_EVENT_PAGEFAULT event: ");
+           printf("flags = %"PRIx64"; ", msg->arg.pagefault.flags);
+           printf("address = %"PRIx64"\n", msg->arg.pagefault.address);
+
+           /* Copy the page pointed to by 'page' into the faulting
+              region. */
+
+           uffdio_copy.src = (unsigned long) page;
+
+           /* We need to handle page faults in units of pages(!).
+              So, round faulting address down to page boundary. */
+
+           uffdio_copy.dst = (unsigned long) msg->arg.pagefault.address &
+                             ~(page_size - 1);
+           uffdio_copy.len = page_size;
+           uffdio_copy.mode = 0;
+           uffdio_copy.copy = 0;
+           if (ioctl(uffd, UFFDIO_COPY, &uffdio_copy) == -1)
+               errExit("ioctl-UFFDIO_COPY");
+
+           printf("(uffdio_copy.copy returned %"PRId64")\n",
+                  uffdio_copy.copy);
+       }
+
+       static void * DM_handler(void *arg)
+       {
+           static struct uffd_msg msg;   /* Data read from userfaultfd */
+           long uffd;                    /* userfaultfd file descriptor */
+           ssize_t nread;
+           uffd = (long) arg;
+
+
            /* Loop, handling incoming events on the userfaultfd
               file descriptor. */
+           struct pollfd pollfd;
+           pollfd.fd = uffd;
+           pollfd.events = POLLIN;
 
-           for (;;) {
-
-               /* See what poll() tells us about the userfaultfd. */
-
-               struct pollfd pollfd;
-               int nready;
-               pollfd.fd = uffd;
-               pollfd.events = POLLIN;
-               nready = poll(&pollfd, 1, -1);
-               if (nready == -1)
-                   errExit("poll");
-
-               printf("\nfault_handler_thread():\n");
-               printf("    poll() returns: nready = %d; "
-                       "POLLIN = %d; POLLERR = %d\n", nready,
-                       (pollfd.revents & POLLIN) != 0,
-                       (pollfd.revents & POLLERR) != 0);
-
+           while (poll(&pollfd, 1, -1) > 0)
+           {
                /* Read an event from the userfaultfd. */
-
                nread = read(uffd, &msg, sizeof(msg));
                if (nread == 0) {
                    printf("EOF on userfaultfd!\n");
                    exit(EXIT_FAILURE);
                }
-
-               if (nread == -1)
+               if (nread == -1) {
                    errExit("read");
-
-               /* We expect only one kind of event; verify that assumption. */
-
-               if (msg.event != UFFD_EVENT_PAGEFAULT) {
-                   fprintf(stderr, "Unexpected event on userfaultfd\n");
-                   exit(EXIT_FAILURE);
                }
-
-               /* Display info about the page-fault event. */
-
-               printf("    UFFD_EVENT_PAGEFAULT event: ");
-               printf("flags = %"PRIx64"; ", msg.arg.pagefault.flags);
-               printf("address = %"PRIx64"\n", msg.arg.pagefault.address);
-
-               /* Copy the page pointed to by 'page' into the faulting
-                  region. Vary the contents that are copied in, so that it
-                  is more obvious that each fault is handled separately. */
-
-               memset(page, 'A' + fault_cnt % 20, page_size);
-               fault_cnt++;
-
-               uffdio_copy.src = (unsigned long) page;
-
-               /* We need to handle page faults in units of pages(!).
-                  So, round faulting address down to page boundary. */
-
-               uffdio_copy.dst = (unsigned long) msg.arg.pagefault.address &
-                                                  ~(page_size - 1);
-               uffdio_copy.len = page_size;
-               uffdio_copy.mode = 0;
-               uffdio_copy.copy = 0;
-               if (ioctl(uffd, UFFDIO_COPY, &uffdio_copy) == -1)
-                   errExit("ioctl-UFFDIO_COPY");
-
-               printf("        (uffdio_copy.copy returned %"PRId64")\n",
-                       uffdio_copy.copy);
+               switch (msg.event) {
+                   case UFFD_EVENT_PAGEFAULT:
+                       handle_page_fault_event(&msg,uffd);
+                       break;
+                   case UFFD_EVENT_FORK:
+                      // handle_event_fork(&msg);
+                       break;
+                   case UFFD_EVENT_REMAP:
+                       break;
+                   case UFFD_FEATURE_EVENT_REMOVE:
+                       break;
+                   default:
+                       break;
+               }
            }
        }
 
@@ -126,21 +118,17 @@
            struct uffdio_register uffdio_register;
            int s;
 
-           char str[30] = "6";
-
-
-
            page_size = sysconf(_SC_PAGE_SIZE);
-           len = strtoull(str, NULL, 0) * page_size;
+           len = NUM_OF_PAGES * page_size;
 
            /* Create and enable userfaultfd object. */
-
            uffd = syscall(__NR_userfaultfd, O_CLOEXEC | O_NONBLOCK);
            if (uffd == -1)
                errExit("userfaultfd");
 
            uffdio_api.api = UFFD_API;
-           uffdio_api.features = 0;
+           uffdio_api.features = UFFD_FEATURE_EVENT_FORK;
+           uffdio_api.ioctls = 0;
            if (ioctl(uffd, UFFDIO_API, &uffdio_api) == -1)
                errExit("ioctl-UFFDIO_API");
 
@@ -168,11 +156,12 @@
 
            /* Create a thread that will process the userfaultfd events. */
 
-           s = pthread_create(&thr, NULL, fault_handler_thread, (void *) uffd);
+           s = pthread_create(&thr, NULL, DM_handler, (void *) uffd);
            if (s != 0) {
                errno = s;
                errExit("pthread_create");
            }
+
 
            /* Main thread now touches memory in the mapping, touching
               locations 1024 bytes apart. This will trigger userfaultfd
