2646e58e3c5c9bd4dcb5ce9bf1f1aa9b8f9305e9
EliorGigi <62066172+EliorGigi@users.noreply.github.com>
2022-05-01 00:12:31 +0300

    Update track_page.c

--- units/track_pages/track_page.c
+++ units/track_pages/track_page.c
@@ -1,14 +1,3 @@
-/*
-The soft-dirty is a bit on a PTE which helps to track which pages a task writes to.
-In order to do this tracking one should
-1. Clear soft-dirty bits from the task’s PTEs.
-   This is done by writing “4” into the /proc/PID/clear_refs file of the task in question.
-2. Wait some time.
-3. Read soft-dirty bits from the PTEs.
-This is done by reading from the /proc/PID/pagemap.
-The bit 55 of the 64-bit qword is the soft-dirty one. If set, the respective PTE was written to since step 1.
- */
-
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -22,7 +11,7 @@ typedef unsigned long long u64;
 #define PME_PRESENT	(1ULL << 63)
 #define PME_SOFT_DIRTY	(1Ull << 55)
 
-#define PAGES_TO_TEST	3
+#define PAGES_TO_TEST	1
 #ifndef PAGE_SIZE
 #define PAGE_SIZE	4096
 #endif
@@ -35,34 +24,25 @@ typedef struct {
     unsigned int present : 1;
 } PagemapEntry;
 
-/* Parse the pagemap entry for the given virtual address.
- *
- * @param[out] entry      the parsed entry
- * @param[in]  pagemap_fd file descriptor to an open /proc/pid/pagemap file
- * @param[in]  vaddr      virtual address to get entry for
- * @return 0 for success, 1 for failure
- */
-int pagemap_get_entry(PagemapEntry *entry, int pagemap_fd, uintptr_t vaddr)
-{
-    size_t nread;
-    ssize_t ret;
-    uint64_t data;
+typedef struct {
+    unsigned int lru : 1;
+    unsigned int active : 1;
+    unsigned int referenced   : 1;
+    unsigned int no_page   : 1;
+    unsigned int unevictable   : 1;
+} KpageFlagsEntry;
 
-    nread = 0;
-    while (nread < sizeof(data)) {
-        ret = pread(pagemap_fd, &data, sizeof(data),
-                    (vaddr / sysconf(_SC_PAGE_SIZE)) * sizeof(data) + nread);
-        nread += ret;
-        if (ret <= 0) {
-            return 1;
-        }
-    }
-    entry->pfn = data & (((uint64_t)1 << 54) - 1);
-    entry->soft_dirty = (data >> 55) & 1;
-    entry->file_page = (data >> 61) & 1;
-    entry->swapped = (data >> 62) & 1;
-    entry->present = (data >> 63) & 1;
-    return 0;
+// inner functions
+void print_page_flags(KpageFlagsEntry *entry) {
+
+    printf("page frame flags: \n");
+    printf("referenced: %d \n",entry->referenced);
+    printf("lru: %d \n",entry->lru);
+    printf("active: %d \n",entry->active);
+    printf("no_page: %d \n",entry->no_page);
+    printf("unevictable: %d \n",entry->unevictable);
+
+    printf("____________ \n");
 }
 
 void print_entry_flags(PagemapEntry *entry) {
@@ -75,24 +55,99 @@ void print_entry_flags(PagemapEntry *entry) {
     printf("____________ \n");
 }
 
-static inline char map_p(u64 map)
+
+/* Parse the pagemap entry for the given virtual address.
+ * @param[out] entry      the parsed entry
+ * @param[in]  pagemap_fd file descriptor to an open /proc/pid/pagemap file
+ * @param[in]  vaddr      virtual address to get entry for
+ * @return 0 for success, 1 for failure
+ */
+int pagemap_get_entry(PagemapEntry *entry, int pagemap_fd, uintptr_t vaddr)
 {
-	return map & PME_PRESENT ? 'p' : '-';
+    size_t nread;
+    ssize_t ret;
+    uint64_t data;
+    nread = 0;
+    while (nread < sizeof(data)) {
+        ret = pread(pagemap_fd, &data, sizeof(data),
+                    (vaddr / sysconf(_SC_PAGE_SIZE)) * sizeof(data) + nread);
+        nread += ret;
+        if (ret <= 0) {
+            return 1;
+        }
+    }
+    entry->present = (data >> 63) & 1;
+
+    if (entry->present != 0) {
+        entry->pfn = data & 0x007fffffffffffff;
+        entry->soft_dirty = (data >> 55) & 1;
+        entry->file_page = (data >> 61) & 1;
+        entry->swapped = (data >> 62) & 1;
+    } else {
+        perror("page not presented");
+        exit(1);
+    }
+
+    return 0;
+}
+
+/* Parse the relevant bits from kpageflags entry for the given pfn.
+ * @param[out] entry      the parsed entry
+ * @param[in]  pfn        the given page frame number
+ * @return 0 for success, 1 for failure
+ */
+int get_page_flags_lru(KpageFlagsEntry *entry, uint64_t pfn)
+{
+    int fd;
+    fd = open("/proc/kpageflags", O_RDONLY);
+    if (fd < 0) {
+        perror("Can't open kpageflags");
+        exit(1);
+    }
+    size_t nread;
+    ssize_t ret;
+    uint64_t data;
+    nread = 0;
+    while (nread < sizeof(data)) {
+        ret = pread(fd, &data, 8,
+                    pfn*8);
+        nread += ret;
+        if (ret <= 0) {
+            return 1;
+        }
+    }
+    entry->referenced = (data >> 2) & 1;
+    entry->lru = (data >> 5) & 1;
+    entry->active = (data >> 6) & 1;
+    entry->no_page = (data >> 20) & 1;
+    entry->unevictable = (data >> 18) & 1;
+    close(fd);
+    return 0;
 }
 
 
-static void clear_refs(void)
-{
-	int fd;
-	char *v = "4";
 
-	fd = open("/proc/self/clear_refs", O_WRONLY);
-	if (write(fd, v, 3) < 3) {
-		perror("Can't clear soft-dirty bit");
-		exit(1);
-	}
-	close(fd);
+/* read and print the relevant kpageflags.
+ * @param[in]  vaddr  page virtual adress of the page
+ */
+void read_kflags(uintptr_t vaddr)
+{
+    int fd;
+    fd = open("/proc/self/pagemap", O_RDONLY);
+    if (fd < 0) {
+        perror("Can't open pagemap");
+        exit(1);
+    }
+
+    PagemapEntry page_map_entry;
+    KpageFlagsEntry page_flags_entry;
+    pagemap_get_entry(&page_map_entry,fd,vaddr);
+    uint64_t pfn = page_map_entry.pfn;
+    get_page_flags_lru(&page_flags_entry,pfn);
+    print_page_flags(&page_flags_entry);
+    close(fd);
 }
+
 
 int main(void)
 {
@@ -100,29 +155,12 @@ int main(void)
 
 	mem = mmap(NULL, PAGES_TO_TEST * PAGE_SIZE,
 			PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, 0, 0);
+    mem[1] = 'c';
+    void * ptr = &mem[1];
+    printf("go to sleep - 2  sec\n");
+    sleep(2);
+    read_kflags(ptr);
 
-	mem[0] = 'c';
-    int fd;
-    fd = open("/proc/self/pagemap", O_RDONLY);
-    if (fd < 0) {
-        perror("Can't open pagemap");
-        exit(1);
-    }
-    //expecting dirty and presented =1
-    PagemapEntry entry;
-    pagemap_get_entry(&entry,fd,mem);
-    print_entry_flags(&entry);
 
-    //expecting dirty = 0
-	clear_refs();
-
-    pagemap_get_entry(&entry,fd,mem);
-    print_entry_flags(&entry);
-
-    //expecting dirty get back to 1
-
-    mem[0] = 'c';
-    pagemap_get_entry(&entry,fd,mem);
-    print_entry_flags(&entry);
 	return 0;
 }
